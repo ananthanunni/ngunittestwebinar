30a02e0f5160fe2875a607e705ad1b3944b3458e
Ananthan Unni <ananthanunni@live.com>
2020-11-04 18:20:24 +0530

    Basic routing and guarding.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  {path:''}
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((r) => r.LoginModule),
+  },
+  {
+    path: 'secure',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./secure-area/secure-area.module').then(
+        (r) => r.SecureAreaModule
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'secure',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
