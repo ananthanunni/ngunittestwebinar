ba422f4717a9de9272ccac2e862878b7f9101119
Ananthan Unni <ananthanunni@live.com>
2020-11-08 12:17:58 +0530

    TODO: Fix broken reactive form validation.

--- src/app/login/components/login/login.component.spec.ts
+++ src/app/login/components/login/login.component.spec.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -29,5 +31,53 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Validate login button disabled', () => {
+    let userIdInput: DebugElement;
+    let passwordInput: DebugElement;
+    let loginButton: DebugElement;
+
+    beforeEach(() => {
+      const inputFields = fixture.debugElement.queryAll(By.css('input'));
+      userIdInput = inputFields[0];
+      passwordInput = inputFields[1];
+      loginButton = fixture.debugElement.query(By.css('button[type=submit]'));
+    });
+
+    it('should not enable login button if user id is empty', () => {
+      userIdInput.nativeElement.value = '';
+      passwordInput.nativeElement.value = 'nonemptypassword';
+
+      userIdInput.triggerEventHandler('change', {});
+      passwordInput.triggerEventHandler('change', {});
+
+      expect(loginButton.nativeElement.disabled).toBeTruthy();
+    });
+
+    it('should not enable login button if password is empty', () => {
+      userIdInput.nativeElement.value = 'validusername';
+      passwordInput.nativeElement.value = '';
+
+      userIdInput.triggerEventHandler('change', {});
+      passwordInput.triggerEventHandler('change', {});
+
+      expect(loginButton.nativeElement.disabled).toBeTruthy();
+    });
+
+    it('should enable login button if fields have value', async () => {
+      userIdInput.nativeElement.value = 'validusername';
+      passwordInput.nativeElement.value = 'validpassword';
+
+      userIdInput.triggerEventHandler('change', {});
+      passwordInput.triggerEventHandler('change', {});
+
+      component.loginForm.updateValueAndValidity();
+
+      fixture.detectChanges();
+      await fixture.whenStable();
+
+      expect(loginButton.nativeElement.disabled).toBeTruthy();
+    });
   });
 });
