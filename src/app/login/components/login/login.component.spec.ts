cbe510715746540f07fe652cdd9e3b0c2d0fc495
Ananthan Unni <ananthanunni@live.com>
2020-11-08 14:09:30 +0530

    Login component unit tests for successful and failed login. Clear button.

--- src/app/login/components/login/login.component.spec.ts
+++ src/app/login/components/login/login.component.spec.ts
@@ -1,4 +1,10 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -6,6 +12,16 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthenticatedUser } from '../../services/authenticated-user';
+import { of, throwError } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { NgModule } from '@angular/core';
+import { Constants } from 'src/app/utils/constants';
+
+@NgModule({
+  id: 'mock-component',
+})
+class MockComponent {}
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -16,7 +32,9 @@ describe('LoginComponent', () => {
       imports: [
         HttpClientTestingModule,
         ReactiveFormsModule,
-        RouterTestingModule,
+        RouterTestingModule.withRoutes([
+          { path: 'secure', component: MockComponent },
+        ]),
         MatSnackBarModule,
         NoopAnimationsModule,
       ],
@@ -75,22 +93,82 @@ describe('LoginComponent', () => {
     });
   });
 
-  it('should call validate api with correct values', () => {
-    const serviceSpy = spyOn(
-      component['authenticationService'],
-      'authenticate$'
-    );
+  describe('validate login button click', () => {
+    let serviceSpy: jasmine.Spy;
+    const mockUser: AuthenticatedUser = {
+      name: 'mockuser',
+      userId: 'mockuserid',
+    };
 
+    beforeEach(() => {
+      serviceSpy = spyOn(component['authenticationService'], 'authenticate$');
+    });
+
+    it('should call validate api with correct values', () => {
+      component.loginForm.controls.userId.setValue('validusername');
+      component.loginForm.controls.password.setValue('nonemptypassword');
+      const loginButton = fixture.debugElement.query(
+        By.css('button[type=submit]')
+      );
+      loginButton.triggerEventHandler('click', {});
+
+      expect(serviceSpy).toHaveBeenCalledWith({
+        userId: 'validusername',
+        password: 'nonemptypassword',
+      });
+    });
+
+    it('should handle successful authentication', fakeAsync(async () => {
+      const localStorageSpy = spyOn(
+        localStorage,
+        'setItem'
+      ).and.callFake(() => {});
+      const routerSpy = spyOn(component['router'], 'navigate');
+
+      // a delay is required for the system to see the change and react.
+      serviceSpy.and.returnValue(of(mockUser).pipe(delay(1000)));
+      const loginButton = fixture.debugElement.query(
+        By.css('button[type=submit]')
+      );
+      loginButton.triggerEventHandler('click', {});
+      fixture.detectChanges();
+      await fixture.whenStable();
+      expect(loginButton.nativeElement.innerText).toBe('Logging in...');
+      tick(1000);
+      fixture.detectChanges();
+      expect(loginButton.nativeElement.innerText).toBe('Login');
+      expect(localStorageSpy).toHaveBeenCalledWith(
+        Constants.authenticatedUserLocalStorageKey,
+        JSON.stringify(mockUser)
+      );
+      expect(routerSpy).toHaveBeenCalledWith(['/secure']);
+    }));
+
+    it('should handle failed authentication', () => {
+      serviceSpy.and.returnValue(throwError('Login rejected'));
+      const snackBarSpy = spyOn(component['snackbar'], 'open');
+
+      fixture.debugElement
+        .query(By.css('button[type=submit]'))
+        .triggerEventHandler('click', {});
+      expect(snackBarSpy).toHaveBeenCalledWith(
+        'Login rejected',
+        '',
+        Constants.snackBarErrorConfig
+      );
+    });
+  });
+
+  it('should clear fields when clicking clear button', () => {
     component.loginForm.controls.userId.setValue('validusername');
     component.loginForm.controls.password.setValue('nonemptypassword');
-    const loginButton = fixture.debugElement.query(
-      By.css('button[type=submit]')
-    );
-    loginButton.triggerEventHandler('click', {});
 
-    expect(serviceSpy).toHaveBeenCalledWith({
-      userId: 'validusername',
-      password: 'nonemptypassword',
-    });
+    // selects first matched item
+    const clearButton = fixture.debugElement.query(By.css('button'));
+    clearButton.triggerEventHandler('click', {});
+
+    const inputs = fixture.debugElement.queryAll(By.css('input'));
+    expect(inputs[0].nativeElement.value).toBeFalsy();
+    expect(inputs[1].nativeElement.value).toBeFalsy();
   });
 });
