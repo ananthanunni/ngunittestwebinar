ea1340e2a0e14100762d6f98f7b6a42e8d8f17ac
Ananthan Unni <ananthanunni@live.com>
2020-11-08 16:48:13 +0530

    ListItemComponent unit tests.

--- src/app/secure-area/components/list-item/list-item.component.spec.ts
+++ src/app/secure-area/components/list-item/list-item.component.spec.ts
@@ -1,10 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Color } from '../../services/color';
 
 import { ListItemComponent } from './list-item.component';
 
 describe('ListItemComponent', () => {
   let component: ListItemComponent;
   let fixture: ComponentFixture<ListItemComponent>;
+  const mockColor: Color = {
+    color: '#ddd',
+    id: 1,
+    name: 'whatever',
+    pantoneValue: 'ac',
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,10 +23,46 @@ describe('ListItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListItemComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not display apply button if the selected color is the current color', () => {
+    component.color = mockColor;
+    component.selectedColor = mockColor;
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.query(By.css('button'))).toBeFalsy();
+  });
+
+  it('should display apply button if the selected color is the current color', () => {
+    component.color = mockColor;
+    component.selectedColor = {
+      color: '#fff',
+      name: 'anothercolor',
+      id: 2,
+      pantoneValue: 'xyz',
+    } as Color;
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.query(By.css('button'))).toBeTruthy();
+  });
+
+  it('should emit select event when clicking the button', () => {
+    const eventSpy = spyOn(component['select'], 'emit');
+    component.color = mockColor;
+    component.selectedColor = {
+      color: '#fff',
+      name: 'anothercolor',
+      id: 2,
+      pantoneValue: 'xyz',
+    } as Color;
+    fixture.detectChanges();
+    fixture.debugElement
+      .query(By.css('button'))
+      .triggerEventHandler('click', {});
+    expect(eventSpy).toHaveBeenCalledWith(mockColor);
   });
 });
