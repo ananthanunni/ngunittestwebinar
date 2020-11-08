1dbc3017cf0a514790e0c634bc58677e6d43ea6a
Ananthan Unni <ananthanunni@live.com>
2020-11-08 16:28:02 +0530

    AuthGuard unit tests.

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,10 +1,15 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AuthenticatedUser } from '../login/services/authenticated-user';
 
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
+  const mockUser: AuthenticatedUser = {
+    name: 'mockUser',
+    userId: 'user',
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,7 +18,21 @@ describe('AuthGuard', () => {
     guard = TestBed.inject(AuthGuard);
   });
 
-  it('should be created', () => {
+  it('should be created', fakeAsync(() => {
     expect(guard).toBeTruthy();
+  }));
+
+  it('should allow navigation to pass if localstorage has value', () => {
+    spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify(mockUser));
+
+    expect(guard.canActivate(null, null)).toBeTruthy();
+  });
+
+  it('should not allow navigation and should redirect to login screen if localstorage has no value', () => {
+    spyOn(localStorage, 'getItem').and.returnValue('');
+    const routerSpy = spyOn(guard['router'], 'navigate');
+
+    expect(guard.canActivate(null, null)).toBeFalsy();
+    expect(routerSpy).toHaveBeenCalledWith(['/login']);
   });
 });
