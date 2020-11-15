d4a8d4762ea708ad1053e995e57fa08b61d3f984
Ananthan Unni <ananthanunni@live.com>
2020-11-15 11:45:53 +0530

    Http utils unit tests.

--- src/app/utils/httpUtils.spec.ts
+++ src/app/utils/httpUtils.spec.ts
@@ -2,5 +2,16 @@ import { HttpUtils } from './httpUtils';
 
 describe('HttpUtils tests', () => {
   // TODO: Add a unit test to handle api path without leading slash
+  it('should not add extra slash if input alredy has a slash', () => {
+    expect(HttpUtils.toApiUrl('/someendpoint/someapi')).toBe(
+      `${HttpUtils.baseUrl}/someendpoint/someapi`
+    );
+  });
+
   // TODO: Add a unit test to handle api path WITH leading slash
+  it('should not add extra slash if input alredy has a slash', () => {
+    expect(HttpUtils.toApiUrl('someendpoint/someapi')).toBe(
+      `${HttpUtils.baseUrl}/someendpoint/someapi`
+    );
+  });
 });
