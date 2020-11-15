fecf1cd39bffaf557356edc4b9bee1191e4bff09
Ananthan Unni <ananthanunni@live.com>
2020-11-15 12:17:16 +0530

    List item unit tests.

--- src/app/secure-area/components/list-item/list-item.component.spec.ts
+++ src/app/secure-area/components/list-item/list-item.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Color } from '../../services/color';
 
 import { ListItemComponent } from './list-item.component';
 
@@ -15,10 +17,63 @@ describe('ListItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListItemComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not display apply button if selected color is current color', () => {
+    const fakeColor: Color = {
+      color: '#fff',
+    } as Color;
+
+    component.color = fakeColor;
+    component.selectedColor = fakeColor;
+
+    // important! make the change detector run
+    fixture.detectChanges();
+
+    // Find the button;
+    const applyButton = fixture.debugElement.query(By.css('button'));
+    expect(applyButton).toBeFalsy();
+  });
+
+  it('should display apply button if selected color is different', () => {
+    const fakeColor1: Color = {
+      color: '#fff',
+    } as Color;
+
+    const fakeColor2 = { color: '#ccc' } as Color;
+
+    component.color = fakeColor1;
+    component.selectedColor = fakeColor2;
+
+    // important! make the change detector run
+    fixture.detectChanges();
+
+    // Find the button;
+    const applyButton = fixture.debugElement.query(By.css('button'));
+    expect(applyButton).toBeTruthy();
+  });
+
+  it('should display apply button if selected color is different', () => {
+    const eventSpy = spyOn(component.select, 'emit');
+
+    const fakeColor1: Color = {
+      color: '#fff',
+    } as Color;
+    const fakeColor2 = { color: '#ccc' } as Color;
+
+    component.color = fakeColor1;
+    component.selectedColor = fakeColor2;
+
+    // important! make the change detector run
+    fixture.detectChanges();
+
+    // Find the button;
+    const applyButton = fixture.debugElement.query(By.css('button'));
+    applyButton.triggerEventHandler('click', {});
+    expect(eventSpy).toHaveBeenCalledWith(fakeColor1);
   });
 });
